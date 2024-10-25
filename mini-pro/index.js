a26e71ccd27b1e7366e0512eec38577c389853a6
DANIELA MENEZES GUIMARÃES <120280682+Daniela912-del@users.noreply.github.com>
2024-10-25 14:31:48 -0300

    Add files via upload

---
+++ mini-pro/index.js
@@ -0,0 +1,34 @@
+const on = document.getElementById('on');
+ const off = document.getElementById( 'off');
+ const lamp = document.getElementById('lamp');
+
+ function isLampBroken(){
+    return lamp.src.indexOf('quebrada')>-1
+ }
+
+  function lampOn () {
+   if (! isLampBroken ()){
+ 
+  
+    lamp.src='./img/ligada.jpg';
+  }
+}
+    function lampOff (){
+      if (! isLampBroken ()){
+
+        lamp.src= './img/desligada.jpg'
+    }
+   }
+        function mouseover (){
+
+        }
+ function lamBroken(){
+    lamp.src ='./img/quebrada.jpg'
+ }
+
+ on.addEventListener('click',lampOn);
+ off.addEventListener('click',lampOff);
+  lamp.addEventListener('mouseover',lampOn);
+  lamp.addEventListener('mouseleave',lampOff);
+ lamp.addEventListener('dblclick',lamBroken);
+
